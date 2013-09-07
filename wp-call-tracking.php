71b9994133738b0b067ee0bc96e832f97004673b
Michael Chacon <mike@sicdigital.com>
2013-09-07 00:30:54 -0400

    First Real Commit, added basic file structure and plugin header

---
+++ wp-call-tracking.php
@@ -0,0 +1,14 @@
+
+<?php   
+/* 
+Plugin Name: WP Call Tracking
+Plugin URI: http://www.sicdigital.com
+Description: A Premium Plugin for tracking phone calls genereated through your website.
+Author: Michael Chaocn
+Version: 1.0 
+Author URI: http://www.mikechacon.com
+*/  
+
+define('WPCT_DIR', plugin_dir_url(__FILE__) );
+define( 'WPCT_PATH', plugin_dir_path(__FILE__) );
+
