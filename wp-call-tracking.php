266529d085427cd6adf2ed43002b162cb8490d65
Michael Chacon <mike@sicdigital.com>
2013-09-07 00:58:49 -0400

    Added Twilio PHP library to plugin library

--- wp-call-tracking.php
+++ wp-call-tracking.php
@@ -1,5 +1,4 @@
-
-<?php   
+<?php
 /* 
 Plugin Name: WP Call Tracking
 Plugin URI: http://www.sicdigital.com
@@ -7,8 +6,22 @@ Description: WP Call Tracking is the most advanced plugin available for  trackin
 Author: Michael Chaocn
 Version: 1.0 
 Author URI: http://www.mikechacon.com
+
+
+Version: 1.0 
+
+This is all about getting this plugin to be functional, no user interface.
+Focus is on functionality of core aspects listed below.
+
+1. The ability to collect and Store Calls
+2. Store Meta data of calls (Recordings, Date, Status, CallerID, )
+3. Sorting Calls based on Date/Time
+
 */  
 
+//Set Plugin Paths
 define('WPCT_DIR', plugin_dir_url(__FILE__) );
 define( 'WPCT_PATH', plugin_dir_path(__FILE__) );
 
+//Include Twilio
+require(WPCT_PATH . '/library/twilio/Services/Twilio.php');
