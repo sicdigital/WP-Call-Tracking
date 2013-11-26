a568c667eb25ca5efbd3c9bf37bd94685ee62aec
Michael Chacon <mike@sicdigital.com>
2013-11-25 19:46:30 -0500

    Commiting old updates

--- wp-call-tracking.php
+++ wp-call-tracking.php
@@ -6,79 +6,19 @@ Description: WP Call Tracking is the most advanced plugin available for  trackin
 Author: Michael Chaocn
 Version: 1.0 
 Author URI: http://www.mikechacon.com
-
-
-Version: 1.0 
-
-This is all about getting this plugin to be functional, no user interface.
-Focus is on functionality of core aspects listed below.
-
-1. The ability to collect and Store Calls
-2. Store Meta data of calls (Recordings, Date, Status, CallerID, )
-3. Sorting Calls based on Date/Time
-
 */  
 
 //Set Plugin Paths
-define('WPCT_DIR', plugin_dir_url(__FILE__) );
+define('WPCT_DIR', plugin_dir_url(__FILE__) );	
 define( 'WPCT_PATH', plugin_dir_path(__FILE__) );
+
+// set your AccountSid and AuthToken from www.twilio.com/user/account
+$AccountSid = "AC9ca3f5e6b5058706c8dbecb07a25964d";
+$AuthToken = "1733429de4c33df71c083ab9c5e088fa";
 
 //Include Twilio
 require(WPCT_PATH . '/library/twilio/Services/Twilio.php');
-
-// set your AccountSid and AuthToken from www.twilio.com/user/account
-$AccountSid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
-$AuthToken = "YYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY";
-
-function wpct_scripts() {
-	wp_enqueue_style( 'admin-styles', WPCT_DIR . '/interface/wpct-styles.css' );
-}
-
-add_action( 'admin_enqueue_scripts', 'wpct_scripts' );	
-
-add_action('admin_menu', 'wpct_menu_page');
-
-function wpct_menu_page() {
-	
-  add_menu_page('WPCT Settings', 'Calls', 'manage_options', 'wpct', 'wpct_dashboard', WPCT_DIR . '/interface/images/phone-icon.png');
-  add_submenu_page( 'wpct', 'Numbers', 'Numbers', 'manage_options', 'wpct-numbers', 'wpct_numbers_page');
-  add_submenu_page( 'wpct', 'Settings page title', 'Settings', 'manage_options', 'wpct-settings', 'wpct_settings_page');
-}
-
-function wpct_dashboard(){
-                echo '<div class="wrap"><div id="icon-options-general" class="icon32"><br></div>
-                <h2>Settings</h2></div>';
-}
-
-function wpct_numbers_page(){
-                echo '<div class="wrap"><div id="icon-options-general" class="icon32"><br></div>
-                <h2>FAQ</h2></div>';
-}
-function wpct_settings_page(){
-	
-	if(!empty($_REQUEST['twilio_sid']) && !empty($_REQUEST['twilio_token']))
-		{
-			$twilio_sid = $_REQUEST['twilio_sid'];
-			$twilio_token = $_REQUEST['twilio_token'];
-
-			update_option("twilio_sid", $twilio_sid );
-			update_option("twilio_token", $twilio_token );
-		}
-
-$twilio_sid_option=get_option("twilio_sid");
-$twilio_token_option=get_option("twilio_token");
-
-?>      
-	<form name="frm_rss" action="<?=get_option("siteurl")?>/wp-admin/admin.php?page=twilio"   method="post" enctype="multipart/form-data">
-	<label for="twilio_sid">SID</label>
-	<input type="text" name="twilio_sid" id="sid" size="65" value="<?=$twilio_sid_option?>"/>
-	
-	<label for="twilio_sid">Token</label>
-	<input type="text" name="twilio_token" id="token" size="65" value="<?=$twilio_token_option?>"/>
-	<input type="submit" value="SAVE" name="twilio_submit" />  
-	</form>
-
-<?php }
+require(WPCT_PATH . '/settings/wpct-admin.php');
 
 function wpct_post_types() {
   $labels = array(
